d1c53be06b65a4cd60a52d0f38cc1c4b2db172fe
Anna Kwadrans <akwadran@c2r17s6.42madrid.com>
2025-11-30 21:01:55 +0100

    validate map

--- src/parsing/parse_file.c
+++ src/parsing/parse_file.c
@@ -6,7 +6,7 @@
 /*   By: akwadran <akwadran@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 14:42:07 by kegonza           #+#    #+#             */
-/*   Updated: 2025/11/30 18:43:54 by akwadran         ###   ########.fr       */
+/*   Updated: 2025/11/30 20:58:53 by akwadran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,22 +42,103 @@ static int	parse_textures(char **lines, t_game *game, int *map_index)
 	return (0);
 }
 
+static int	validate_chars(char *line)
+{
+	int	i;
+	//bool	found;
+
+	i = 0;
+	//found = 0;
+	//while (line[i] == ' ')
+	//	i++;
+	while (line[i])
+	{
+		printf("CHAR CHECK: %c\n", line[i]);
+		//found = 1;
+		if (!(line[i] == '1' || line[i] == '0' || line[i] == 'N' || line[i] == 'S'
+			|| line[i] == 'E' || line[i] == 'W' || line[i] == ' '))
+		{
+			printf("INCORRECT CHAR: %s\n", &line[i]);
+			return (error("Map contains incorrect character"));
+		}
+		i++;
+	}
+	//while (line[i])
+	//{
+	//	if (line[i] != ' ')
+	//		return (error("Map contains incorrect character"));
+	//	i++;
+	//}
+	//if (!found)
+	//	return (error("Map can´t contain empty line"));
+	return (0);
+}
+
+static int	find_player(char *line)
+{
+	int	i;
+	int	player;
+
+	i = 0;
+	player = 0;
+	while (line[i])
+	{
+		if (line[i] == 'S' || line[i] == 'N' || line[i] == 'E' || line[i] == 'W')
+			player++;
+		i++;
+	}
+	return (player);
+}
+
+static int	get_map_height(char **lines, int map_index, t_map *map)
+{
+	int	i;
+
+	i = map_index;
+	while (lines[i])
+		i++;
+	map->height = i - map_index;
+	return (0);
+}
+
+static int	check_walls(char **lines, t_map *map)
+{
+	// por hacer
+	(void)map;
+	(void)lines;
+	return (0);
+}
+
 static int	parse_map(char **lines, t_game *game, int map_index)
 {
 	int	i;
-	int width;
+	int	player;
+	t_map	*map;
 
+	map = (t_map *)malloc(sizeof(t_map));
+	if (!map)
+		return (error("Map: Malloc failed"));
+	map->width = 0;
+	map->height = 0;
 	i = map_index;
-	width = 0;
+	player = 0;
 	while (!strcmp(lines[i], "\n"))
 		i++;
 	map_index = i;
-	
 	while (lines[i])
 	{
-		if (ft_strlen(lines[i]) > width)
-			width = ft_strlen(lines[i]);
+		if ((int)ft_strlen(lines[i]) > map->width)
+			map->width = ft_strlen(lines[i]);
+		if (validate_chars(lines[i]))
+			return (1);
+		player += find_player(lines[i]);
+		i++;
 	}
+	if (player != 1)
+		return (error("There must be exactly one player"));
+	get_map_height(lines, map_index, map);
+	check_walls(lines, map);
+	(void)game; // QUITAR
 	return (0);
 }
 
