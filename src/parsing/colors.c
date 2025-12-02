ffa7f3e5b60c07774ec4d25a9622fc837ccc0e27
Anna Kwadrans <akwadran@c2r2s6.42madrid.com>
2025-12-02 21:01:41 +0100

    working parser

---
+++ src/parsing/colors.c
@@ -0,0 +1,65 @@
+#include "../../includes/src.h"
+
+static bool	valid_range(int color[3])
+{
+	if (color[0] >= 0 && color[0] <= 255 && color[1] >= 0 && color[1] <= 255
+		&& color[2] >= 0 && color[2] <= 255)
+		return (1);
+	return (0);
+}
+
+int	get_color(char *line, t_game *game)
+{
+	char	**color;
+	int		i;
+	int		color_index;
+	int		rgb[3];
+	unsigned int	trgb;
+
+	if (!line || !game)
+		return (1);
+	i = 1;
+	while (ft_isspace(line[i]))
+		i++;
+	color_index = i;
+	while (line[i])
+	{
+		if (!(ft_isdigit(line[i]) || line[i] == ',' || ft_isspace(line[i])))
+		{
+			// err: incorrect color format
+			return (1);
+		}
+		i++;
+	}
+	color = ft_split(&line[color_index], ',');
+	if (!color)
+	{
+		// err: no color
+		return (1);
+	}
+	i = 0;
+	while (color[i])
+		i++;
+	if (i > 3)
+	{
+		// err: incorrect color format
+		return (1);
+	}
+	rgb[0] = ft_atoi(color[0]);
+	rgb[1] = ft_atoi(color[1]);
+	rgb[2] = ft_atoi(color[2]);
+	//printf("RGB: %d,%d,%d\n", rgb[0], rgb[1], rgb[2]);
+	free_array(color);
+	if (!valid_range(rgb))
+	{
+		// err: invalid range
+		return (1);
+	}
+	trgb = (rgb[0] << 16 | rgb[1] << 8 | rgb[2]);
+	//printf("COLOR UNS INT: %x\n", trgb);
+	if (!strncmp(line, "F", 1))
+		game->config.floor_color = trgb;
+	else
+		game->config.ceil_color = trgb;
+	return (0);
+}
--- src/parsing/parse_file.c
+++ src/parsing/parse_file.c
@@ -6,7 +6,7 @@
 /*   By: akwadran <akwadran@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 14:42:07 by kegonza           #+#    #+#             */
-/*   Updated: 2025/11/30 21:48:57 by akwadran         ###   ########.fr       */
+/*   Updated: 2025/12/02 21:00:01 by akwadran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,176 +42,27 @@ static int	parse_textures(char **lines, t_game *game, int *map_index)
 	return (0);
 }
 
-static int	validate_chars(char *line)
-{
-	int	i;
-
-	i = 0;
-	while (line[i])
-	{
-		if (!(line[i] == '1' || line[i] == '0' || line[i] == 'N' || line[i] == 'S'
-			|| line[i] == 'E' || line[i] == 'W' || line[i] == ' '))
-		{
-			printf("INCORRECT CHAR: %s\n", &line[i]); // QUITAR
-			return (error("Map contains incorrect character"));
-		}
-		i++;
-	}
-	return (0);
-}
-
-static int	find_player(char *line)
-{
-	int	i;
-	int	player;
-
-	i = 0;
-	player = 0;
-	while (line[i])
-	{
-		if (line[i] == 'S' || line[i] == 'N' || line[i] == 'E' || line[i] == 'W')
-			player++;
-		i++;
-	}
-	return (player);
-}
-
-static int	check_walls(char **lines, t_map *map)
-{
-	// por hacer
-	(void)map;
-	(void)lines;
-	return (0);
-}
-
-static char	*rm_trailing_newline(char *line)
-{
-	char	*new;
-
-	if (line[ft_strlen(line) - 1] == '\n')
-	{
-		new = (char *)malloc(sizeof(char) * ft_strlen(line));
-		ft_strlcpy(new, line, ft_strlen(line));
-	}
-	else
-		new = ft_strdup(line);
-	return (new);
-}
-
-static int	get_grid(char **lines, int map_index, t_map *map)
-{
-	int	i;
-	char	**grid;
-
-	map->height = 0;
-	i = map_index;
-	while (lines[i])
-	{
-		map->height++;
-		i++;
-	}
-	if (map->height == 0)
-		return (error("Empty map"));
-	grid = (char**)malloc(sizeof(char *) * map->height + 1);
-	if (!grid)
-		return (error("Grid: Malloc failed"));
-	i = 0;
-	while (lines[map_index])
-	{
-		grid[i] = rm_trailing_newline(lines[map_index]);
-		i++;
-		map_index++;
-	}
-	grid[i] = NULL;
-	map->grid = grid;
-	return (0);
-}
-
-static int	fill_empty_spaces(t_map *map)
-{
-	int	i;
-	char	*tmp;
-	int	j;
-
-	i = 0;
-	while (map->grid[i])
-	{
-		if ((int)ft_strlen(map->grid[i]) < map->width)
-		{
-			tmp = (char *)malloc(sizeof(char) * (map->width + 1));
-			ft_strlcpy(tmp, map->grid[i], ft_strlen(map->grid[i]));
-			j = ft_strlen(map->grid[i]);
-			while (j < map->width)
-			{
-				tmp[j] = ' ';
-				j++;
-			}
-			tmp[j] = '\0';
-			free(map->grid[i]);
-			map->grid[i] = tmp;
-		}
-		i++;
-	}
-	return (0);
-}
-
-static int	parse_map(char **lines, t_game *game, int map_index)
-{
-	int	i;
-	int	player;
-	t_map	*map;
-
-	map = (t_map *)malloc(sizeof(t_map));
-	if (!map)
-		return (error("Map: Malloc failed"));
-	map->width = 0;
-	map->height = 0;
-	i = map_index;
-	player = 0;
-	while (!strcmp(lines[i], "\n"))
-		i++;
-	map_index = i;
-	if (get_grid(lines, map_index, map))
-		return (1);
-	i = 0;
-	while (map->grid[i])
-	{
-		if ((int)ft_strlen(map->grid[i]) > map->width)
-			map->width = ft_strlen(map->grid[i]);
-		if (validate_chars(map->grid[i]))
-			return (1);
-		player += find_player(map->grid[i]);
-		i++;
-	}
-	if (player != 1)
-		return (error("There must be exactly one player"));
-	fill_empty_spaces(map);
-	check_walls(map->grid, map);
-	game->config.map = map;
-	return (0);
-}
-
 int	parse_file(char *file, t_game *game)
 {
-	char	**lines;
+	char	**buffer;
 	int		map_index;
 
-	lines = get_lines(file);
-	if (!lines)
+	buffer = get_buffer(file);
+	if (!buffer)
 		return (error("Buffer: Malloc failed"));
-	print_array(lines); // QUITAR
-	if (parse_textures(lines, game, &map_index))
+	//print_array(buffer); // QUITAR
+	if (parse_textures(buffer, game, &map_index))
 	{
-		free_array(lines);
+		free_array(buffer);
 		return (1);
 	}
-	if (parse_map(lines, game, map_index))
+	if (parse_map(buffer, game, map_index))
 	{
 		//err
-		free_array(lines);
+		free_array(buffer);
 		return (1);
 	}
-	print_config(&game->config);
-	free_array(lines);
+	print_config(&game->config); // QUITAR
+	free_array(buffer);
 	return (0);
 }
