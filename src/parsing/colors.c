9454b046bc72e303d79a39c6f6d569c66f6d4620
Anna Kwadrans <akwadran@c2r2s6.42madrid.com>
2025-12-02 22:34:57 +0100

    divide get_color acc. to norm

--- src/parsing/colors.c
+++ src/parsing/colors.c
@@ -8,12 +8,50 @@ static bool	valid_range(int color[3])
 	return (0);
 }
 
-int	get_color(char *line, t_game *game)
+static bool	valid_chars(char *line, int color_index)
+{
+	while (line[color_index])
+	{
+		if (!(ft_isdigit(line[color_index]) || line[color_index] == ','
+			|| ft_isspace(line[color_index])))
+		{
+			// err: incorrect color format
+			return (0);
+		}
+		color_index++;
+	}
+	return (1);
+}
+
+static int	convert_color(char *line, int color_index, unsigned int *trgb)
 {
 	char	**color;
 	int		i;
-	int		color_index;
 	int		rgb[3];
+
+	color = ft_split(&line[color_index], ',');
+	if (!color)
+		return (error("Color: Malloc failed"));
+	i = 0;
+	while (color[i])
+		i++;
+	if (i > 3)
+		return (error("Incorrect color format"));
+	rgb[0] = ft_atoi(color[0]);
+	rgb[1] = ft_atoi(color[1]);
+	rgb[2] = ft_atoi(color[2]);
+	//printf("RGB: %d,%d,%d\n", rgb[0], rgb[1], rgb[2]);
+	free_array(color);
+	if (!valid_range(rgb))
+		return (error("Incorrect color format"));
+	*trgb = (rgb[0] << 16 | rgb[1] << 8 | rgb[2]);
+	return (0);
+}
+
+int	get_color(char *line, t_game *game)
+{
+	int		i;
+	int		color_index;
 	unsigned int	trgb;
 
 	if (!line || !game)
@@ -22,41 +60,10 @@ int	get_color(char *line, t_game *game)
 	while (ft_isspace(line[i]))
 		i++;
 	color_index = i;
-	while (line[i])
-	{
-		if (!(ft_isdigit(line[i]) || line[i] == ',' || ft_isspace(line[i])))
-		{
-			// err: incorrect color format
-			return (1);
-		}
-		i++;
-	}
-	color = ft_split(&line[color_index], ',');
-	if (!color)
-	{
-		// err: no color
+	if (!valid_chars(line, color_index))
+		return (error("Incorrect color format"));
+	if (convert_color(line, color_index, &trgb))
 		return (1);
-	}
-	i = 0;
-	while (color[i])
-		i++;
-	if (i > 3)
-	{
-		// err: incorrect color format
-		return (1);
-	}
-	rgb[0] = ft_atoi(color[0]);
-	rgb[1] = ft_atoi(color[1]);
-	rgb[2] = ft_atoi(color[2]);
-	//printf("RGB: %d,%d,%d\n", rgb[0], rgb[1], rgb[2]);
-	free_array(color);
-	if (!valid_range(rgb))
-	{
-		// err: invalid range
-		return (1);
-	}
-	trgb = (rgb[0] << 16 | rgb[1] << 8 | rgb[2]);
-	//printf("COLOR UNS INT: %x\n", trgb);
 	if (!strncmp(line, "F", 1))
 		game->config.floor_color = trgb;
 	else
