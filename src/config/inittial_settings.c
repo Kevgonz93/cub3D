4df8bd6c872ac7c524114420b9625d543a9e986d
ania <kwadranss@gmail.com>
2026-01-06 16:21:37 +0100

    fix parsing

--- src/config/inittial_settings.c
+++ src/config/inittial_settings.c
@@ -27,8 +27,8 @@ void	init_config(t_config *config)
 	config->so_tex = NULL;
 	config->we_tex = NULL;
 	config->ea_tex = NULL;
-	config->floor_color = -1;
-	config->ceil_color = -1;
+	config->floor_color = 0;
+	config->ceil_color = 0;
 	config->fov = PI / 3;
 	config->map = NULL;
 }
